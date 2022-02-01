d94b44813fd786f3eb26333c8306d025f8415a5e
Charles Peace <charlespeace@hotmail.com>
2022-01-31 16:23:17 -0800

    started writing script.js by adding variables to reference html and outlining basic functions

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var quizContainter = document.getElementById('quiz')
+var resultsContainer = document.getElementById('results')
+var startButton = document.getElementById('start')
+
+
+function startQuiz(){}
+function buildQuiz(){}
+function showResults(){}
+
+startButton.addEventListener ('click', startQuiz)
