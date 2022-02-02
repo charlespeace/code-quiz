5957b5a3e00a9ad751d0ea9fa74faf3dc437f0e7
Charles Peace <charlespeace@hotmail.com>
2022-02-01 16:57:08 -0800

    added quiz.html and highscore.html, reorganized html functionality

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,40 @@
-var quizContainter = document.getElementById('quiz')
-var resultsContainer = document.getElementById('results')
-var startButton = document.getElementById('start')
+// Create variables to reference html elements
+var quizContainter = document.querySelector('#quiz')
+var resultsContainer = document.querySelector('#results')
+var startButton = document.querySelector('#start')
 
-
-function startQuiz(){}
-function buildQuiz(){}
-function showResults(){}
-
-startButton.addEventListener ('click', startQuiz)
-
+// Create variable to store questions/answers
 var questions = [
     {
         question: "How many are there?",
         choice: ["one", "two", "three", "four"],
-        answer: "c"
+        answer: "one"
     },
     {
         question: "How many are there?",
         choice: ["one", "two", "three", "four"],
-        answer: "c"
+        answer: "one"
     },
     {
         question: "How many are there?",
         choice: ["one", "two", "three", "four"],
-        answer: "c"
+        answer: "one"
     },
     {
         question: "How many are there?",
         choice: ["one", "two", "three", "four"],
-        answer: "c"
+        answer: "one"
     },
-],
+]
+
+// Function to start quiz
+function startQuiz(){}
+// Function to build quiz results based on user input
+function buildQuiz(){}
+
+// Function to display results on screen once done
+function showResults(){}
+
+buildQuiz();
+// Add listener to start button to begin quiz
+startButton.addEventListener ("click", startQuiz)
